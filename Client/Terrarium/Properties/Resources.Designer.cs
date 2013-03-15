33bd10ed6d4009ba423ff82e5cdc2a47dfabce43
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-03-15 12:53:10 -0400

    convert to vs2012

--- Client/Terrarium/Properties/Resources.Designer.cs
+++ Client/Terrarium/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Terrarium.Forms.Properties {
+namespace Terrarium.Client.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Terrarium.Forms.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Terrarium.Forms.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Terrarium.Client.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -57,6 +57,16 @@ namespace Terrarium.Forms.Properties {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap screensaver {
+            get {
+                object obj = ResourceManager.GetObject("screensaver", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
